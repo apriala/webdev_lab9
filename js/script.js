514e3538757c0978d6be0dc34802f899c8f33b6d
apriala <apriala@hawk.iit.edu>
2024-11-05 18:57:10 -0600

    add to script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 function init(){
-	var button = document.getElementById('entrybutton');
+  var button = document.getElementById('entrybutton');
  
+  function showMeText(){
+    var textbox = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = textbox.value;
+    alert("Alexander Priala: " + textbox.value);
+  }
 
-
-
-
-
-
-
-window.addEventListener('load', init);
+}
